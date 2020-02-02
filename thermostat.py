50b500bccdbb1bfb37017a485ae00b983fb4f29d
Chris Hoffman <chris.c.hoffman@gmail.com>
2020-02-01 22:28:49 -0500

    pull max temp setting from firestore and log temps there

--- thermostat.py
+++ thermostat.py
@@ -3,11 +3,13 @@
 import time
 import board
 import adafruit_dht
-import sys
 import paho.mqtt.client as mqtt
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
  
 prevOn = None
-
 def offon(on):
     global prevOn
     if prevOn is None or prevOn != on:
@@ -17,11 +19,39 @@ def offon(on):
         client.publish("test/message/cmnd/heater/power", on );
         client.disconnect()
         prevOn = on
- 
+
+def log(temp, humidity):
+    global db
+    print("Temp: {:.1f} F | Humidity: {}% ".format(temp, humidity))
+    now = datetime.now()
+    doc_ref = db.collection(u'temps').document(now.strftime(u'%Y-%m-%d %H:%M:%S'))
+    doc_ref.set({
+        u'datetime': now,
+        u'temp': temp,
+        u'humidity': humidity,
+    })
+
+def onConfigChange(doclist, changes, read_time):
+    global maxTemp
+    global minTemp
+    maxTemp = doclist[0].get('temp')
+    print(u'temp set to {}'.format(maxTemp))
+    minTemp = maxTemp - 2
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'projectId': 'heater-267000',
+})
+db = firestore.client()
+doc_ref = db.collection(u'config').document(u'maxTemp')
+doc_ref.on_snapshot(onConfigChange)
+
 dhtDevice = adafruit_dht.DHT22(board.D4)
 last5Temps = []
-minTemp = int(sys.argv[1])
-maxTemp = int(sys.argv[2])
+maxTemp = doc_ref.get().get('temp')
+print(u'initial temp {}'.format(maxTemp))
+minTemp = maxTemp - 2
 
 while True:
     try:
@@ -29,11 +59,9 @@ while True:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
-        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
-              .format(temperature_f, temperature_c, humidity))
+        log(temperature_f, humidity)
         last5Temps = last5Temps[-4:] + [temperature_f]
         avgOfLast5 = sum(last5Temps) / len(last5Temps)
-        print(avgOfLast5, last5Temps)
         if avgOfLast5 < minTemp:
             offon(1)
         elif avgOfLast5 > maxTemp:
