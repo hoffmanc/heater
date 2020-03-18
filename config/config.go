ad5e3cced9180a848f9afd1c9836ef7d3df291e1
Chris Hoffman <chris.c.hoffman@gmail.com>
2020-03-18 07:55:09 -0400

    add auth and systemd file for config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+type AuthCreds struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func check(c *gin.Context, e error) {
 	if e != nil {
@@ -19,16 +27,37 @@ func readTemp(c *gin.Context) {
 	c.HTML(200, "config.tmpl", gin.H{"temp": string(dat)})
 }
 
+func getCreds(creds *AuthCreds) error {
+	if credsTxt, err := ioutil.ReadFile(os.Getenv("AUTH_CREDS")); err != nil {
+		return err
+	} else {
+		if err := json.Unmarshal(credsTxt, &creds); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
 	r := gin.Default()
 	r.LoadHTMLFiles("config.tmpl")
 
-	r.GET("/config", readTemp)
+	var creds AuthCreds
+	if err := getCreds(&creds); err != nil {
+		panic(err)
+	}
 
-	r.POST("/config", func(c *gin.Context) {
+	log.Println(creds)
+
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{creds.Username: creds.Password}))
+
+	authorized.GET("/config", readTemp)
+
+	authorized.POST("/config", func(c *gin.Context) {
 		err := ioutil.WriteFile("temp.conf", []byte(c.PostForm("newTemp")), 0664)
 		check(c, err)
 		readTemp(c)
 	})
+
 	r.Run(":8080")
 }
