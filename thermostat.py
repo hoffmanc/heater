b4c830c9ea66c490ef8d49cbbc3a1be99fb5e0a5
Chris Hoffman <chris.c.hoffman@gmail.com>
2020-03-18 13:02:15 -0400

    finish moving to local storage/conf from firestore

--- thermostat.py
+++ thermostat.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+import os
 import time
 import board
 import adafruit_dht
 import paho.mqtt.client as mqtt
 from tinydb import TinyDB
 from datetime import datetime
- 
+
 status = None
 def offon(on):
     global status
@@ -22,46 +23,43 @@ def log(temp, humidity):
     global db, status
     print("Temp: {:.1f} F | Humidity: {}% ".format(temp, humidity))
     now = datetime.now()
-    doc_ref.insert({
-        u'datetime': now,
+    db.insert({
+        u'datetime': now.strftime(u'%Y-%m-%d %H:%M:%S%z'),
         u'temp': temp,
         u'humidity': humidity,
         u'status': status
     })
 
-def onConfigChange(doclist, changes, read_time):
-    global maxTemp
-    global minTemp
-    maxTemp = doclist[0].get('temp')
-    print(u'temp set to {}'.format(maxTemp))
-    minTemp = maxTemp - 2
+def getTempConf():
+    global tempConf
+    t = int(open("config/temp.conf", "r").read())
+    print(u'temp set at {}'.format(t))
+    return t
 
 db = TinyDB(datetime.now().strftime(u'temps-%Y-%m-%d.json'))
 
 last5Temps = []
-maxTemp = int(open("config/temp.conf", "r").read())
-print(u'initial temp {}'.format(maxTemp))
-minTemp = maxTemp - 2
 
 dhtDevice = adafruit_dht.DHT22(board.D4)
 
 while True:
     try:
-        # Print the values to the serial port
+        maxTemp = getTempConf()
+        minTemp = maxTemp - 2
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
         log(temperature_f, humidity)
         last5Temps = last5Temps[-4:] + [temperature_f]
         maxOfLast5 = max(last5Temps)
+
         if maxOfLast5 < minTemp:
             offon(1)
         elif maxOfLast5 > maxTemp:
             offon(0)
-            
- 
+
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
- 
+
     time.sleep(2.0)
